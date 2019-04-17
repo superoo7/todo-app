85bd68874dee5092b477154373694e45061f6254
Lai Weng Han <jlwhoo7@gmail.com>
2019-04-17 09:57:54 +0800

    add SomeVueComponent

---
+++ src/components/SomeVueComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>Some Vue Component</div>
+</template>
+
+<script>
+export default {
+  // name: 'ComponentName',
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style></style>
+
+<test lang="jest">
+  import SomeVueComponent from "../SomeVueComponent.vue";
+  import { mount } from "@vue/test-utils";
+  describe("Some Vue Component", () => {
+    it("snapshot vue component", () => {
+      const vueEl = mount(SomeVueComponent);
+      expect(vueEl.html()).toMatchSnapshot();
+    })
+  })
+</test>
---
+++ src/pages/SomeVueComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+    <some-vue-component />
+  </q-page>
+</template>
+
+<script>
+import SomeVueComponentVue from "../components/SomeVueComponent.vue";
+
+export default {
+  // name: 'PageName',
+  components: {
+    "some-vue-component": SomeVueComponentVue
+  }
+};
+</script>
+
+<style></style>
